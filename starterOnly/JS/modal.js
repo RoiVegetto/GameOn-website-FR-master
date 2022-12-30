05bb8a82586fb76ef5056e3bee62f32627f177fa
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2022-12-30 02:33:57 +0100

    feat : suppression des erreurs a la fermeture de la modal

--- starterOnly/JS/modal.js
+++ starterOnly/JS/modal.js
@@ -12,17 +12,29 @@ const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
 const formData = document.querySelectorAll('.formData');
 const modalClose = document.querySelector('span.close');
+const form = document.getElementById('form');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
-
-modalClose.addEventListener('click', closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = 'block';
 }
 
+modalClose.addEventListener('click', closeModal);
+
 function closeModal() {
+  const error = document.querySelectorAll('.error');
+  error.forEach(function (element) {
+    element.style.display = 'none';
+  });
+  formFirst.classList.remove('error-border');
+  formLast.classList.remove('error-border');
+  formEmail.classList.remove('error-border');
+  formBirthdate.classList.remove('error-border');
+  formQuantity.classList.remove('error-border');
+
   modalbg.removeAttribute('style');
+  form.reset();
 }
